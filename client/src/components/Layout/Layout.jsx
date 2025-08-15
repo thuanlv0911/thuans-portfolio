1057e669043b35dda0347ecda673837c857b2913
thuanlv0911 <thuanlv0911@gmail.com>
2025-08-15 14:37:30 +0700

    feat: 04-home

---
+++ client/src/components/Layout/Layout.jsx
@@ -0,0 +1,35 @@
+import Home from "../../pages/Home/Home";
+import { GoSidebarExpand, GoSidebarCollapse } from "react-icons/go";
+import "./layout.css";
+import { useState } from "react";
+import Menus from "../Menus/Menus";
+
+export default function Layout() {
+
+    const [toggle, setToggle] = useState(true);
+
+    const handleToggle = () => {
+        setToggle(!toggle);
+    }
+
+    return (
+        <>
+            <div className="sidebar-section">
+                <div className={toggle ? "sidebar-toggle sidebar" : "sidebar"}>
+                    <div className="sidebar-toggle-icons ">
+
+                        <p onClick={handleToggle} className="toggle-icon">
+                            {toggle ? (<GoSidebarExpand className="hover:text-blue-500" size={30} />) :
+                                (<GoSidebarCollapse size={30} className="hover:text-blue-500" />)}
+                        </p>
+                    </div>
+                    <Menus toggle={toggle} />
+                </div>
+                <div className="container">
+                    <Home />
+                </div>
+            </div >
+
+        </>
+    )
+}
