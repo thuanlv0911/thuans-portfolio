40fea5c6022555f6669ec0df0bbeec6a4d9c7140
thuanlv0911 <thuanlv0911@gmail.com>
2025-08-18 17:29:44 +0700

    chore: setup certifications

---
+++ client/src/pages/Certifications/Certifications.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import "./certification.css"
+
+const Certifications = () => {
+    return (
+        <div className="min-h-screen bg-[#10101A] flex items-center justify-center">
+            <div className="projects w-full px-4 py-8">
+                <h1 className="text-4xl font-mono certifications-title">Certifications</h1>
+                <div className="certification-container gap-6 mt-8">
+                    <div className="certification-item bg-[#0B0B13] rounded-lg shadow-lg p-4">
+                        hi
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Certifications
